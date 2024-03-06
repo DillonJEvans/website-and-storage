bbc648a4fb69eefe48adc5d20332215ee9e95552
DillonJEvans <dillonjayevans@gmail.com>
2024-03-06 12:33:51 -0800

    Finished rearranging models, and added debugging messages on startup

--- models/database/__init__.py
+++ models/database/__init__.py
@@ -1,19 +1,56 @@
 import os
 
-from azure.cosmos import CosmosClient, PartitionKey
+from azure.cosmos import CosmosClient, DatabaseProxy, ContainerProxy, PartitionKey
 
 
-# Create the cosmos client.
-client = CosmosClient(os.getenv('COSMOS_HOST'), {'masterKey': os.getenv('COSMOS_KEY')})
-# Get (or create) the cosmos database.
-database = client.create_database_if_not_exists(os.getenv('COSMOS_DATABASE_ID'))
-# Get (or create) the cosmos container.
-container = database.create_container_if_not_exists(
-  os.getenv('COSMOS_CONTAINER_ID'),
-  PartitionKey('/lastName'),
-  offer_throughput=1000,
-  unique_key_policy={'uniqueKeys': [{'paths': ['/firstName', '/lastName']}]}
-)
+def create_client() -> CosmosClient:
+  """
+  Creates the client for connecting to Cosmos.
+  :returns: The client.
+  """
+  account_name = os.getenv('COSMOS_ACCOUNT_NAME')
+  key = os.getenv('COSMOS_KEY')
+  print(f'COSMOS: Creating the client ({account_name})...', end='')
+  client = CosmosClient(
+    url=f'https://{account_name}.documents.azure.com:443/',
+    credential={'masterKey': key}
+  )
+  print(' success.')
+  return client
+
+
+def get_or_create_database() -> DatabaseProxy:
+  """
+  Gets the database if it exists, or creates it if it doesn't.
+  :returns: The database.
+  """
+  database_id = os.getenv('COSMOS_DATABASE_ID')
+  print(f'COSMOS: Getting/creating the database ({database_id})...', end='')
+  database = client.create_database_if_not_exists(database_id)
+  print(' success.')
+  return database
+
+
+def get_or_create_container() -> ContainerProxy:
+  """
+  Gets the container if it exists, or creates it if it doesn't.
+  :returns: The container.
+  """
+  container_id = os.getenv('COSMOS_CONTAINER_ID')
+  print(f'COSMOS: Getting/creating the container ({container_id})...', end='')
+  container = database.create_container_if_not_exists(
+    id=container_id,
+    partition_key=PartitionKey('/lastName'),
+    offer_throughput=1000,
+    unique_key_policy={'uniqueKeys': [{'paths': ['/firstName', '/lastName']}]}
+  )
+  print(' success.')
+  return container
+
+
+client = create_client()
+database = get_or_create_database()
+container = get_or_create_container()
 
 
 from .add_people import add_people
--- models/object_storage/__init__.py
+++ models/object_storage/__init__.py
@@ -1,26 +1,49 @@
 import os
 
-from azure.storage.blob import BlobServiceClient
+from azure.storage.blob import BlobServiceClient, ContainerClient, ContainerProperties
 
 
-# Create the blob client.
-print('Connecting to the blob service...', end='')
-client = BlobServiceClient(
-  account_url=f'https://{os.getenv("BLOB_ACCOUNT_NAME")}.blob.core.windows.net',
-  credential={'account_name': os.getenv('BLOB_ACCOUNT_NAME'), 'account_key': os.getenv('BLOB_KEY')}
-)
-print(' connected.')
-# Get or create the blob container.
-# public_access='blob'????
-print('Getting the blob container...', end='')
-container = client.get_container_client(os.getenv('BLOB_CONTAINER_ID'))
-if container.exists():
-  print(' blob container retrieved.')
-else:
-  print('\nBlob container does not exist.')
-  print('Creating the blob container...', end='')
-  container.create_container()
-  print(' blob container created.')
+def create_client() -> BlobServiceClient:
+  """
+  Creates the client for connecting to the blob service.
+  :returns: The client.
+  """
+  account_name = os.getenv('BLOB_ACCOUNT_NAME')
+  print(f'BLOB: Creating the client ({account_name})...', end='')
+  key = os.getenv('BLOB_KEY')
+  client = BlobServiceClient(
+    account_url=f'https://{account_name}.blob.core.windows.net',
+    credential=key
+  )
+  print(' success.')
+  return client
+
+
+def get_or_create_container() -> ContainerClient:
+  """
+  Gets the container if it exists, or creates it if it doesn't.
+  :returns: The container.
+  """
+  container_id = os.getenv('BLOB_CONTAINER_ID')
+  print(f'BLOB: Getting the container ({container_id})...', end='')
+  # The ContainerProperties constructor always sets `name` to None,
+  # even if name is provided. This is a workaround.
+  container_properties = ContainerProperties()
+  container_properties.name = container_id
+  container_properties.public_access = 'blob'
+  container = client.get_container_client(container_properties)
+  if container.exists():
+    print(' success.')
+  else:
+    print(f'\nBLOB: Container ({container_id}) does not exist.')
+    print(f'BLOB: Creating the container ({container_id})...', end='')
+    container.create_container()
+    print(' success.')
+  return container
+
+
+client = create_client()
+container = get_or_create_container()
 
 
 from .save_object import save_object
